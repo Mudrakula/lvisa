5b69265f02360ae4119adc4f3877f8147601cec7
rbezryk <rodion.bezryk@devsbs.com>
2017-03-13 21:40:28 +0200

    Add text.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  var screenHeight = $('.screen').height();
   $('#main-menu a').click(function() {
     var id = $(this).attr('href');
     $('body').animate({scrollTop: $(id).offset().top}, 300);
@@ -6,5 +7,13 @@ $(document).ready(function() {
 
   $('#top-btn').click(function() {
     $('body').animate({scrollTop: 0}, 300);
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > screenHeight / 2) {
+      $('#top-btn').show();
+    } else {
+      $('#top-btn').hide();
+    }
   });
 });
