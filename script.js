98b859a1a4480bff2e6cd944fc3b00f5a545e619
Mudrakula <mavpa_skm@mail.ru>
2017-04-20 17:34:59 +0300

    Fix grid. Clients slider.

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
   var screenHeight = $('.screen').height();
   var move = function(direction = 1) {
-    let curPosition = $('.clients-slider').position().left;
-    $('.clients-slider').css('left', (curPosition + direction*100) +'px');
+    let slider = $('.clients-slider');
+    let curPosition = slider.position().left;
+    let newPosition = curPosition + direction*100;
+    console.log($(window).width() - slider.width());
+    if (direction > 0 && newPosition > 0)
+      newPosition = 0
+    else if (direction < 0 && newPosition < $(window).width() - slider.width())
+      newPosition = $(window).width() - slider.width();
+
+    slider.css('left', newPosition +'px');
   };
 
   $('#main-menu a').click(function() {
@@ -25,6 +33,7 @@ $(document).ready(function() {
   var moving;
   $('.client-slider-arrow').mouseenter(function() {
     var direction = $(this).data('direction');
+    let curPosition = $('.clients-slider').position().left;
     move(direction);
     moving = setInterval(function() {
       move(direction);
@@ -33,5 +42,29 @@ $(document).ready(function() {
 
   $('.client-slider-arrow').mouseleave(function() {
     clearInterval(moving);
+  });
+
+  $('.clients-slider img').click(function() {
+    $('.clients-slider img').removeClass('current');
+    $(this).addClass('current');
+    $('#imageModal img').attr('src', this.src);
+    $('#imageModal').modal();
+  });
+
+  $('.modalArrow').click(function() {
+    let newImg;
+    let currentImg = $('.clients-slider .current');
+    if ($(this).data('direction') > 0)
+      newImg = currentImg.next('img');
+    else
+      newImg = currentImg.prev('img');
+
+    if (newImg.length) {
+      currentImg.removeClass('current');
+      newImg.addClass('current');
+    } else
+      return;
+
+    $('#imageModal img').attr('src', newImg.attr('src'));
   });
 });
